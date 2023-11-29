cb0452a73978825a1a6c87c67aa0300a888b05ac
Akhil Anto Tharayil <atharayil@iplit.in>
2023-11-29 17:49:22 +0530

    Arabic print fix

---
+++ bahmni_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bahmni Custom',
+    'version': '1.0',
+    'summary': 'Custom product module to meet bahmni requirement',
+    'sequence': 1,
+    'description': """
+Bahmni Product
+====================
+""",
+    'category': 'Custom',
+    'website': '',
+    'images': [],
+    'depends': ['product', 'product_expiry'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
