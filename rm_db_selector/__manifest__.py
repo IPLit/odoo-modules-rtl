4cc54399fb7932fa5e7b226c03efa08fd002ffe9
Akhil Anto Tharayil <atharayil@iplit.in>
2023-12-29 12:36:34 +0530

    rm_db_selector

---
+++ rm_db_selector/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Remove manage DB selector',
+    'version': '10.0',
+    'summary': 'Removes manage DB selector button from home page',
+    'description': "",
+    'author': 'Akhil',
+    'category': 'Hidden',
+    'sequence' : 1,
+    'depends': ['base'],
+    'data': [
+        
+        'views/webclient_templates.xml',
+
+    ],
+    'demo': [],
+    'images': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb':[],
+}
